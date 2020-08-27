903a972d03304fa1b2df69bc5ff0795a1f5c8d3b
Karine Dias <harlequinuniform@gmail.com>
2020-08-27 20:51:35 +0200

    Update script for vscode extensions

--- vscode/install-vscode-extensions.sh
+++ vscode/install-vscode-extensions.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Visual Studio Code :: Package list
-packages=(
-    nicoespeon.abracadabra,
-    aaron-bond.better-comments,
-    gruntfuggly.todo-tree,
-    alefragnani.bookmarks,
-    formulahendry.code-runner,
-    ms-python.python-2020,
-    kevinrose.vsc-python-indent,
-    ms-azuretools.vscode-docker,
-    davidanson.vscode-markdownlint,
+declare -a packages=("nicoespeon.abracadabra"
+    "aaron-bond.better-comments"
+    "gruntfuggly.todo-tree"
+    "alefragnani.bookmarks"
+    "formulahendry.code-runner"
+    "ms-python.python"
+    "kevinrose.vsc-python-indent"
+    "ms-azuretools.vscode-docker"
+    "davidanson.vscode-markdownlint"
 )
 
 for i in ${packages[@]}; do
   code --install-extension $i
 done
+
