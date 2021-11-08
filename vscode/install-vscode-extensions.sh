8349d088a40cf356a5d3f593a1439f180f07fd96
Karine <harlequinuniform@gmail.com>
2021-11-08 12:19:35 +0100

    Add new extensions and modify VSCode settings accordingly

--- vscode/install-vscode-extensions.sh
+++ vscode/install-vscode-extensions.sh
@@ -4,12 +4,17 @@
 declare -a packages=("nicoespeon.abracadabra"
     "aaron-bond.better-comments"
     "gruntfuggly.todo-tree"
+	  "wayou.vscode-todo-highlight"
     "alefragnani.bookmarks"
     "formulahendry.code-runner"
     "ms-python.python"
     "kevinrose.vsc-python-indent"
     "ms-azuretools.vscode-docker"
     "davidanson.vscode-markdownlint"
+	  "coenraads.bracket-pair-colorizer-2"
+	  "shardulm94.trailing-spaces"
+	  "streetsidesoftware.code-spell-checker"
+	  "george-alisson.html-preview-vscode"
 )
 
 for i in ${packages[@]}; do
